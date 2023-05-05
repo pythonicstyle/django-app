a2f0f401ce15a5afd02af7bcc0a808784a06dedb
Anton Garanin <antongaranin78@gmail.com>
2023-05-05 23:05:40 +0300

    serializers added, myapiapp created

--- M_13_IntroductionToDjangoRESTFramework/mysite/mysite/urls.py
+++ M_13_IntroductionToDjangoRESTFramework/mysite/mysite/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('req/', include('requestdataapp.urls')),
+    path('api/', include('myapiapp.urls')),
 ]
 
 urlpatterns += i18n_patterns(
--- M_13_IntroductionToDjangoRESTFramework/mysite/shopapp/views.py
+++ M_13_IntroductionToDjangoRESTFramework/mysite/shopapp/views.py
@@ -7,9 +7,57 @@ from django.views import View
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .forms import ProductForm, OrderForm, GroupForm
 from .models import Product, Order, ProductImage
+from .serializers import ProductSerializer, OrderSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [
+        SearchFilter,
+        DjangoFilterBackend,
+        OrderingFilter,
+    ]
+    search_fields = ["name", "description"]
+    filterset_fields = [
+        "name",
+        "description",
+        "price",
+        "discount",
+        "archived",
+        "created_by",
+    ]
+    ordering_fields = [
+        "name",
+        "price",
+        "discount",
+    ]
+
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    filter_backends = [
+        SearchFilter,
+        DjangoFilterBackend,
+        OrderingFilter,
+    ]
+    filterset_fields = [
+        "delivery_address",
+        "promocode",
+        "user",
+    ]
+    ordering_fields = [
+        "created_at",
+        "delivery_address",
+    ]
+
 
 class ShopIndexView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
